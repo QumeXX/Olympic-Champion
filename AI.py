7d05f099e849d2d35cfa535a7b63bfc8b8db30fe
QumeXX <moderatormondy@gmail.com>
2025-09-14 00:27:54 +0300

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,15 +1,11 @@
 from openai import OpenAI
+from Data import API_KEY
 
 def AI_func(msg):
-  with open("Token.txt", "r") as f:
-    TOKEN = f.read()
-    f.close()
-
   client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key=TOKEN,
+    api_key=API_KEY,
   )
-
   completion = client.chat.completions.create(
     model="deepseek/deepseek-chat-v3-0324:free",
     messages=[
